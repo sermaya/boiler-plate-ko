dd7185b72924c3eb49d2361107a106b89bf8b2fc
junelee <junelee@ebay.com>
2020-12-21 22:22:57 +0900

    login and generate token

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name : {
@@ -30,6 +33,45 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 });
+
+userSchema.pre('save', function(next){
+    var user = this;
+
+    //password가 변경될 때만 암호화 실행
+    if (user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err);
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+})
+
+userSchema.methods.comparePassword = function (plainPassword, callback){
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if (err) return callback(err);
+        callback(null, isMatch);
+    })
+}
+
+userSchema.methods.generateToken = function (callback){
+    var user = this;
+
+    //jsonwebtoken을 이용해서 token을 생성하기
+    var token = jwt.sign(user._id.toHexString(),'secretToken');
+
+    user.token = token;
+    user.save(function(err, user){
+        if(err) return callback(err);
+        callback(null, user);
+    });
+}
 
 const User = mongoose.model('user', userSchema)
 
